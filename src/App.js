5b1f2b823b5b26c83056575b6b664f5bd8cde8e0
Arthur Coelho <arthurcoelho7885@gmail.com>
2022-08-03 18:07:19 -0300

    feat: home page created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
+      <Routes>
+        <Route path="/" element={ Home } />
+      </Routes>
     </div>
   );
 }
